f37edd1073e39d6197bf1b24820ec32f4afd4e09
brvhjustice <62227490+brvhjustice@users.noreply.github.com>
2021-12-04 13:56:38 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/views/donate.html", function(req, res){
+    res.sendFile(__dirname + "/views/donate.html")
+})
+
+app.get("/views/donate.html", function(req, res) {
+    res.redirect("/")
+})
+
+app.get("/views/success.html", function(req, res){
+    res.sendFile(__dirname + "/views/success.html")
+})
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server is running on port 3000');
+});
